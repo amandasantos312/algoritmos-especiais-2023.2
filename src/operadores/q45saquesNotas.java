c34807b2660b748cfe36031123729f9ffded9616
Amanda-Santos312 <catce.2022111tads0053@aluno.ifpi.edu.br>
2023-08-15 12:01:14 -0300

    feat: questões de operadores

---
+++ src/operadores/q45saquesNotas.java
@@ -0,0 +1,22 @@
+package operadores;
+
+import java.util.Scanner;
+/* Um algoritmo para gerenciar os saques de um caixa eletônico deve possuir algum mecanismo
+para decidir o número de notas de cada valor que deve ser disponibilizado para o cliente que realizou o saque.
+Um possível critério seria o da "distribuição ótima" no sentido  de que as notas de menor valor disponíveis fossem
+distribuídas em número mínimo possível. Por exemplo, se a máquina só dispõe de notas de R$ 50, R$ 10, R$ 5 e de R$ 1,
+para uma quantia de solicitada de R$ 87, o algoritmo deveria indicar uma nota de R$ 50, três notas de R$ 10, uma nota de
+R$ 5 e duas notas de R$ 1. Ecreva um algoritmo que receba o valor da quantia solicitada e retorne a distribuição das
+notas de acordo com o critério da distribuição ótima. */
+public class q45saquesNotas {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+
+
+
+        input.close();
+    }
+}
---
+++ src/operadores/q46entradaParcelas.java
@@ -0,0 +1,21 @@
+package operadores;
+
+import java.util.Scanner;
+/* Uma loja vende seus produtos no sistema entrada mais duas prestações,
+sendo a entrada maior ou igual a cada uma das prestações; estas devem ser igual, inteiras e as maiores possíveis.
+Por exemplo, se o valor da mercadoria for R$ 270,00, a entrada e as duas prestações são iguais a R$ 90,00;
+se o valor da mercadoria for R$ 302,00, a entrada é de R$ 102,00 e as duas prestações são iguais a R$ 100,00.
+Escreva um algoritmo que receba o valor da mercadoria e forneça o valor da entrada e das duas prestações,
+de acordo com as regras acima. */
+public class q46entradaParcelas {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+
+
+
+        input.close();
+    }
+}
