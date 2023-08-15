ec25bb3a4ac02284265aeaac59a333dfff00e6ef
Amanda-Santos312 <catce.2022111tads0053@aluno.ifpi.edu.br>
2023-08-14 21:01:23 -0300

    feat: questões de operadores

--- src/operadores/q31binario.java
+++ src/operadores/q31binario.java
@@ -1,16 +1,30 @@
 package operadores;
 
 import java.util.Scanner;
-
+//Leia um número inteiro (4 dígitos binários), calcule e escreva o equivalente na base decimal.
 public class q31binario {
 
     public static void main(String[] args) {
-
+        //Entrada:
         Scanner input = new Scanner(System.in);
 
+        System.out.println("Num1: 1 ou 0");
+        int num1 = input.nextInt();
 
+        System.out.println("Num2: 1 ou 0");
+        int num2 = input.nextInt();
 
+        System.out.println("Num3: 1 ou 0");
+        int num3 = input.nextInt();
 
+        System.out.println("Num4: 1 ou 0");
+        int num4 = input.nextInt();
+
+        //Processamento:
+        int decimal = (num1 * 8 + num2 * 4 + num3 * 2 + num4 * 1);
+
+        //Saída:
+        System.out.printf("Decimal: %d", decimal);
 
         input.close();
     }
