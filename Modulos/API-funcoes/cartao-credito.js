8a4828629704c6e8e7ad2661b7247100588cf117
Amanda-Santos312 <catce.2022111tads0053@aluno.ifpi.edu.br>
2023-08-24 10:19:23 -0300

    questões com funções

--- Modulos/API-funcoes/cartao-credito.js
+++ Modulos/API-funcoes/cartao-credito.js
@@ -20,37 +20,46 @@ function main () {
     console.log('\n-----> P1 <-----')
     
     const pago_p1 = Number(question('\nValor pago : '))
-    const tempo_p1 = Number(question('Meses : '))
+    const meses_atraso_p1 = Number(question('Meses : '))
 
     const taxa = 15
     const rotativo_1 = fatura - pago_p1
 
-    const valor_a_pagar_p1 = calcula_juros_rotativo(rotativo_1, taxa, tempo_p1)
+    const valor_a_pagar_p1 = calcula_juros_rotativo(rotativo_1, taxa, meses_atraso_p1)
+    const fatura_p1 = calcular_fatura_futura(valor_a_pagar_p1, taxa, meses_atraso_p1)
 
     console.log(`\nValor a Pagar : R$ ${valor_a_pagar_p1.toFixed(2)}`)
     console.log(`Valor Residual  : R$ ${rotativo_1.toFixed(2)}`)
-    console.log(`Meses de Atraso : ${tempo_p1}`)
+    console.log(`Meses de Atraso : ${meses_atraso_p1}`)
+    console.log(`Fatura em ${meses_atraso_p1} meses será de ${fatura_p1.toFixed(2)}`)
 
     console.log('\n----------------')
 
     console.log('\n-----> P2 <-----')
 
     const pago_p2 = Number(question('Valor pago: '))
-    const tempo_p2 = Number(question('Meses: '))
+    const meses_atraso_p2 = Number(question('Meses: '))
 
     const rotativo_2 = fatura - pago_p2
  
-    const valor_a_pagar_p2 = calcula_juros_rotativo(rotativo_2, taxa, tempo_p2)
+    const valor_a_pagar_p2 = calcula_juros_rotativo(rotativo_2, taxa, meses_atraso_p2)
+    const fatura_p2 = calcular_fatura_futura(valor_a_pagar_p2, taxa, meses_atraso_p2)
     
     console.log(`\nValor a Pagar : R$ ${valor_a_pagar_p2.toFixed(2)}`)
     console.log(`Valor Residual  : ${rotativo_2}`)
-    console.log(`Meses de Atraso : ${tempo_p2}`)
-
+    console.log(`Meses de Atraso : ${meses_atraso_p2}`)
+    console.log(`Fatura em ${meses_atraso_p2} meses será de ${fatura_p2.toFixed(2)}`)
 }
 
 
 function calcula_juros_rotativo(rotativo, taxa, tempo) {
     return juros_compostos(rotativo, taxa, tempo)
+}
+
+
+function calcular_fatura_futura(valor, taxa, meses) {
+    const valor_futuro = valor + juros_compostos(valor, taxa, meses)
+    return valor_futuro
 }
 
 
