c92dbe46c135fad6c592a4ed22c8a177b5a03b92
Amanda-Santos312 <catce.2022111tads0053@aluno.ifpi.edu.br>
2023-09-15 11:35:27 -0300

    questões de condicionais

--- Modulos/condicionais/Lista2.1/q16-nota-aluno.js
+++ Modulos/condicionais/Lista2.1/q16-nota-aluno.js
@@ -1,32 +1,39 @@
 import { question } from "readline-sync"
 
 function main() {
-    const nota1 = Number(question('\nNota1: '))
-    const nota2 = Number(question('Nota1: '))
+    const nota1 = Number(question('Nota1: '))
+    const nota2 = Number(question('Nota2: '))
 
     const media = calcular_media(nota1, nota2) 
     const situacao = verifica_situacao(media)
    
-    console.log(`Média: ${media}`)
+    console.log(`\nMédia: ${media}`)
     console.log(`Situação: ${situacao}`)
 }
 
 function calcular_media(n1, n2) {
-    return ( n1 + n2 / 2)
+    return ( n1 + n2) / 2
 }
 
 function verifica_situacao(media) {
     if (media >= 7) {
         return 'Aprovado'
     } else {
+        console.log(`Você está de Prova Final!`)
         const exame = Number(question('Nota exame: '))
-        const media_final = (media + exame) / 2
+        const situacao_pf = verifica_situacao_pf(exame, media)
+        return situacao_pf 
+    }
+}
 
-        if (media_final >= 5) {
-            return 'Aprovado'
-        } else {
-            return 'Reprovado'
-        }
+function verifica_situacao_pf(nota, media) {
+    const media_final = calcular_media(nota, media)
+    console.log(`Sua média após a Prova Final: ${media_final.toFixed(1)}`)
+
+    if (media_final >= 5) {
+        return 'Aprovado PF'
+    } else {
+        return 'Reprovado PF'
     }
 }
  
--- Modulos/condicionais/Lista2.1/q17-numeros.js
+++ Modulos/condicionais/Lista2.1/q17-numeros.js
@@ -1,7 +1,40 @@
 import { question } from "readline-sync"
 
 function main() {
+    const num1 = Number(question('Num1: '))
+    const num2 = Number(question('Num2: '))
+
+    const resto = calcula_resto(num1, num2)
+    const opcao = verifica_resto(resto, num1, num2)
     
+    console.log(`Resto: ${resto}`)
+    console.log(`Resultado: ${opcao}`)
+}
+
+function calcula_resto(num1, num2) {
+    return num1 % num2
+}
+
+function verifica_resto(resto, n1, n2) {
+    if (resto === 1) {
+        return n1 + n2 + resto    
+    } else if (resto === 2) {
+        return verifica_nums(n1, n2)
+    } else if (resto === 3) {
+        return (n1 + n2) * n1
+    } else if (resto === 4) {
+        return (n1 + n2) / n2
+    } else {
+        return (n1 * n1) + (n2 * n2)
+    }
+}
+
+function verifica_nums(n1, n2){
+    if (n1 % 2 === 0 && n2 % 2 === 0) {
+        return 'Pares'
+    } else {
+        return 'Impares'
+    }
 }
 
 main()
