7fe17549def8702b542745d5b00b422cb99a0e2f
Amanda-Santos312 <catce.2022111tads0053@aluno.ifpi.edu.br>
2023-10-01 12:45:38 -0300

    questões iterações com while

--- Modulos/iterações/Lista1/q5-fatorial.js
+++ Modulos/iterações/Lista1/q5-fatorial.js
@@ -1,6 +1,17 @@
 import { question } from "readline-sync"
 
 function main() {
+    let numero = Number(question('Numero: '))
 
+    if (numero === 0) return 1
+
+    let fat = 1
+
+    while (numero > 1) {
+        fat = fat * numero
+        numero --
+    }
+    console.log(`Fatorial: ${fat}`)
 }
+
 main()
