35a7ba2659421495a411d588e38ec5e9026d119d
Amanda-Santos312 <catce.2022111tads0053@aluno.ifpi.edu.br>
2023-10-14 22:58:34 -0300

    q22 - not finished

--- Modulos/iterações-while/Lista1.2/q22-bois.js
+++ Modulos/iterações-while/Lista1.2/q22-bois.js
@@ -1,39 +1,25 @@
-import {print, get_number, get_number_min, get_positive_number, get_text, clear_screen, } from "../../utils/inputs.js"
-
+import {print, get_positive_number, get_text} from "../../utils/inputs.js"
+//não finalizei
 function main() {
-    let id_menor
-    let id_maior
-    let peso_menor = 0
-    let peso_maior = 9999
+    let id_menor 
+    let id_maior 
+    let peso_menor = 0 
+    let peso_maior = 9999 
 
-    const qtd_fichas = get_positive_number('\n>>> Qtd de Fichas: ')
-    menu()
-    let opcao = get_number_min(menu())
+    let qtd_fichas = get_positive_number('\n>>> Qtd de Fichas: ')
+    
+    const opcao = get_positive_number(menu(), 1)
 
     while (opcao != 0) {
-        menu()
 
         if (opcao === 1) {
-            cadastrar_bois(qtd_fichas)
+            cadastrar_bois(qtd_fichas, id_menor, id_maior, peso_menor, peso_maior)
         } else if (opcao === 2){
             print("Função em Breve")//mostrar_bois(qtd_fichas)
         } else {
             print(`Opcao Invalida!`)
         }
-        
-        /*if (peso <= peso_menor) {
-            peso_menor = peso
-            id_menor = id
-        }
-
-        if (peso >= peso_maior) {
-            peso_maior = peso
-            id_maior = id
-        }*/
-
-        //print_bois(peso_menor, id_menor, peso_maior, id_maior)
     }
-
 }
 
 function menu() {
@@ -49,19 +35,29 @@ function menu() {
     return menu
 }
 
-function cadastrar_bois(qtd_fichas) {
+function cadastrar_bois(qtd_fichas, id_menor, id_maior, peso_menor, peso_maior) {
     let count = 1
 
     while (count <= qtd_fichas) {
         const id = get_positive_number('\nId: ')
         const nome = get_text('Nome: ')
         const peso = get_positive_number('Peso(kg): ')
-
+        
         count++
+
+        if (peso < peso_menor) {
+            peso_menor = peso 
+            id_menor = id
+        }
+
+        if (peso > peso_maior) {
+            peso_maior = peso
+            id_maior = id
+        }
     }
 }
 
-/*function print_bois(id_maior, peso_maior, id_menor, peso_menor) {
+function print_bois(id_maior, peso_maior, id_menor, peso_menor) {
     const print = `
     Id Maior: ${id_maior}
     Maior Peso: ${peso_maior}
@@ -69,6 +65,6 @@ function cadastrar_bois(qtd_fichas) {
     Menor Peso: ${peso_menor}`
 
     return print
-}*/
+}
 
 main()
