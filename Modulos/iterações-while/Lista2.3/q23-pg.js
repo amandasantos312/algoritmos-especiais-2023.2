ef3d09ae70ed2c39f79fc06688e9c906a5a26517
Amanda-Santos312 <catce.2022111tads0053@aluno.ifpi.edu.br>
2023-10-23 17:35:45 -0300

    questões 23 e 24 - iterações com while

---
+++ Modulos/iterações-while/Lista2.3/q23-pg.js
@@ -0,0 +1,16 @@
+/* Escreva um algoritmo que leia a razão de uma PG (Progressão Geométrica) e o seu primeiro termo e 
+escreva os N termos da PG. Ler o valor de N. */
+
+import { get_positive_number, print } from "../../utils/inputs.js"
+
+function main() {
+    const n = get_positive_number('N: ')
+    const r = get_positive_number('Razao: ')
+    let a0 = get_positive_number('A0: ')
+
+    for (let i = 1; i <= n; i++) {
+        print(a0)
+        a0 *= r
+    }
+}
+main()
