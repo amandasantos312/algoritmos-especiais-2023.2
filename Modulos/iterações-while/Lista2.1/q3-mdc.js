2eaf557b32773c83ec10ee01c65812df122e7814
Amanda-Santos312 <catce.2022111tads0053@aluno.ifpi.edu.br>
2023-11-01 12:42:30 -0300

    deleted

--- Modulos/iterações-while/Lista2.1/q3-mdc.js
+++
@@ -1,6 +0,0 @@
-/* Leia 2 (dois) números, calcule e escreva o mdc (máximo divisor comum) entre os números lidos. */
-
-function main() {
-    
-}
-main()
--- Modulos/iterações-while/Lista2.1/q5-dois-numeros.js
+++
@@ -1,7 +0,0 @@
-/* Leia dois números X e N. A seguir, escreva o resultado das divisões de X por N onde, após cada 
-divisão, X passa a ter como conteúdo o resultado da divisão anterior e N é decrementado de 1 em 1, até chegar a 2. */
-
-function main() {
-    
-}
-main()
