410f56052a419c4204c1db94d45173990a07d1e0
Amanda-Santos312 <catce.2022111tads0053@aluno.ifpi.edu.br>
2023-12-07 21:19:36 -0300

    arquivo principal

--- Modulos/registros-matrizes/PatroCars/app.js
+++ Modulos/registros-matrizes/PatroCars/app.js
@@ -1,12 +1,13 @@
 import { ulid } from 'ulid'
-import { enter_to_continue, print, get_number_min, get_positive_number, get_text, skip_line } from './utils.js'
+import { enter_to_continue, print, get_number_min, get_text, skip_line } from './utils.js'
+import { iniciar_dados, obter_index, verificar_opcao, bubble_sort, gravar_dados_montadoras, gravar_dados_modelos } from './app-utils.js'
 
-
-const montadoras = []
+    let montadoras = iniciar_dados('montadoras')
+    let modelos = iniciar_dados('modelos')
 
 function main() {
-    
-    let opcao = get_number_min(show_menu(), 0)
+
+    let opcao = get_number_min(menu(), 0)
 
     while (opcao != 0) {
 
@@ -14,40 +15,62 @@ function main() {
             const montadora = adicionar_montadora()
             montadoras.push(montadora)
         } else if (opcao === 2) {
-            skip_line()
-            listar_montadoras()
+            listar_montadoras(montadoras)
         } else if (opcao === 3) {
-            atualizar(montadoras)
-        } 
+            atualizar(montadoras, 'montadoras')
+        } else if (opcao === 4) {
+            remover(montadoras, 'montadoras')
+        } else if (opcao === 5) {
+            listar_montadoras()
+            const forma = get_text('\nFiltrar por:\n1- Parte Nome\n2- Parte Nome Pais\n>> ')
+            const filtrados = verificar_opcao(forma, montadoras)
+            const atributo = get_text('Qual Atributo? ')
+            const asc_desc = get_text('ASC || DESC? ')
+            const ordenados = bubble_sort(filtrados, atributo, asc_desc)
+            print(ordenados)
+        } else if (opcao === 6) {
+            const modelo = adicionar_modelo(modelos)
+            modelos.push(modelo)
+        } else if (opcao === 7) {
+            listar_modelos(modelos)
+        } else if (opcao === 8) {
+            atualizar(modelos, 'modelos')
+        } else if (opcao === 9) {
+            remover(modelos, 'modelos')
+        }
 
         enter_to_continue()
         console.clear()
-        opcao = get_positive_number(show_menu())
+        opcao = get_number_min(menu(), 0)
     }
+    gravar_dados_montadoras(montadoras) //assim que sair do while, os dados irão ser gravados!
+    gravar_dados_modelos(modelos)
 }
 
-function show_menu() {
+function menu() {
     const menu = `
     ------> SYSTEM CARS <-------
 
     -----> V1 Montadoras <-----
     1 - Adicionar Montadora 
     2 - Listar Montadoras
-    3 - Atualizar Montadora
-    4 - Remover Montadora
-    5 - Filtrar Montadora
+    3 - Atualizar 
+    4 - Remover 
+    5 - Filtrar e Ordenar
 
     -----> V2 Modelo Veiculos <-----
     6 - Adicionar Modelo
     7 - Listar Modelos
-    8 - Remover Modelo
-    9 - Editar Modelo
+    8 - Atualizar
+    9 - Remover
+    10 - Filtrar e Ordenar
 
     -----> V3 Veiculos <-----
-    10 - Adicionar Veiculo
-    11 - Editar e Remover
-    12 - Vender Veiculo
-    13 - Listar Veiculos
+    11 - Adicionar Veiculo
+    12 - Listar
+    13 - Atualizar
+    14 - Remover
+    15 - Vender
     
     0 - Sair
     
@@ -61,20 +84,108 @@ function adicionar_montadora() {
         'id': ulid(),
         'nome': '',
         'pais': '',
-        'ano_fundacao': 0, 
+        'ano': 0, 
     }
     
     montadora['nome'] = get_text('\nNome: ')
     montadora['pais'] = get_text('Pais: ')
-    montadora['ano_fundacao'] = get_number_min('Ano: ', 1850)
+    montadora['ano'] = get_number_min('Ano: ', 1800)
+
+    print(`\nMontadora ${montadora['nome']} adicionada com sucesso!`)
 
     return montadora
 }
 
 function listar_montadoras() {
+    skip_line()
+    let i = 0
     for (let montadora of montadoras) {
-        print(montadora)
+        i++
+        print(`${montadora['id']} - ${montadora['nome']} - ${montadora['pais']} - ${montadora['ano']}`)
     }
+    print(`\nVoce possui ${i} montadoras cadastradas!`)
+}
+
+function atualizar(montadoras, tipo, modelos) { //genérica
+
+    if (tipo === 'montadoras') {
+        listar_montadoras()
+
+        const index = obter_index(montadoras)
+        const montadora = montadoras[index]
+
+        montadora['nome'] = get_text('Novo nome: ')
+        montadora['pais'] = get_text('Novo pais: ')
+        montadora['ano'] = get_number_min('Novo Ano: ', 1800)
+
+        return montadora
+
+    } else if (tipo === 'modelos') {
+        listar_modelos()
+
+        const index = obter_index(modelos)
+        const modelo = montadoras[index]
+
+        modelo['nome'] = get_text('\nNome: ')
+        modelo['montadora_id'] = get_text('Montadora Id: ')
+        modelo['valor_ref'] = get_number_min('Valor Referencia: ', 0)
+        modelo['motorizacao'] = get_number_min('Motor(cv): ', 50)
+        modelo['turbo'] = get_text('Turbo? T|F: ')
+        modelo['automatico'] = get_text('Automatico? T|F: ')
+
+        return modelo
+    }
+}
+
+function remover(montadoras, tipo, modelos) { //genérica
+    
+    if (tipo === 'montadoras') {
+        listar_montadoras()
+
+        const index = obter_index(montadoras)
+        const montadora = montadoras[index]['nome']
+
+        montadoras.splice(index, 1)
+        print(`Montadora ${montadora} removida com sucesso!`)
+
+    } else if (tipo === 'modelos') {
+        listar_modelos()
+
+        const index = obter_index(modelos)
+        const modelo = modelos[index]['nome']
+
+        modelos.splice(index, 1)
+        print(`Modelo ${modelo} removido com sucesso!`)
+    }
+}
+
+function adicionar_modelo() {
+    for (let i = 0; i < montadoras.length; i++) {
+        print(`${i}: ${montadoras[i]['nome']}`)
+    }
+
+    const index = get_number_min('\nIndex da Montadora: ')
+    const montadora_id = montadoras[index]['id']
+    const modelo = {
+        'id': ulid(),
+        'nome': get_text('Nome do modelo: '),
+        'montadora_id': montadora_id,
+        'valor_ref': get_number_min('Valor Ref: ', 1000),
+        'motorizacao': get_number_min('Motor(cv): ', 50),
+        'turbo': get_text('Turbo(Sim|Nao): '),
+        'automatico': get_text('Automatico(Sim|Nao): ')
+    }
+    return modelo
+}
+
+function listar_modelos(modelos, montadoras) {
+    skip_line()
+    let i = 0
+    for (let modelo of modelos) {
+        i++
+        print(`${modelo['id']} - ${modelo['nome']} - ${modelo['montadora_id']} - ${modelo['valor_ref']} - ${modelo['motorizacao']} - ${modelo['turbo']} - ${modelo['automatico']}`)
+    }
+    print(`\nVoce possui ${i} modelos cadastrados!`)
 }
 
 
